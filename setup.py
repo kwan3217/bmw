b2abd39467dd06074f2479b89b50746de55c3ef6
kwan3217 <kwan3217@gmail.com>
2024-03-05 10:55:46 -0700

    Added setup.py so this can be a package

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='bmw',
+    version='0.1.0',
+    description='Algorithms from the BMW book',
+    url='https://github.com/kwan3217/bmw/',
+    author='kwan3217',
+    author_email='kwan3217@gmail.com',
+    license='BSD 3-clause',
+    packages=['bmw'],
+    install_requires=[
+                      'numpy'
+                      ]
+
+)
